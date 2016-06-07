1afcb86f851f540b4209cf5d501dbb6d2d4e7125
JNMarcos <marcos_joaos@hotmail.com>
2016-06-07 16:16:18 -0300

    Função calcularMedias()
    Função que calcula de uma só vez as médias.

--- src/programa/AlgoritmoBayes.java
+++ src/programa/AlgoritmoBayes.java
@@ -7,11 +7,16 @@ public class AlgoritmoBayes {
 	
 	public static void calcularSoma(int coordenadaVetor, int valorCoordenadaDoVetor){
 		Dados.setarMediaNaCoordenada(coordenadaVetor, Dados.getMedias()[coordenadaVetor] + valorCoordenadaDoVetor);
+		Double valor = Dados.getMedias()[coordenadaVetor];
+		System.out.println(valor);
 	}
 	
 
-	public static void calcularMedia(int coordenadaVetor, int valorCoordenadaDoVetor){
-		
+	public static void calcularMedias(){
+		for (int i = 0; i < Dados.N_ATRIBUTOS; i++){
+			Dados.getMedias()[i] = (Dados.getMedias()[i])/Dados.N_ATRIBUTOS;
+			System.out.println(Dados.getMedias()[i]);
+		}
 	}
 	
 	public static void calcularDesvioPadrao(){
