3d69aa70899b2dce7b6a8d5cea490c18c5449d80
JNMarcos <marcos_joaos@hotmail.com>
2016-06-07 00:18:56 -0300

    Arquivo data.txt e classe Dados
    Adicionado o arquivo data.txt (possui dados do BD).
    
    Classe Dados implementada, que contém como atributo as médias e os
    desvios dos atributos dos vetores.

---
+++ src/programa/Dados.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package programa;
+
+/**
+ * @author JN
+ *
+ */
+public class Dados {
+	
+	private static final int N_ATRIBUTOS = 255;
+	private Double[] medias;
+	private Double[] desviosPadrao;
+	
+	public Dados(){
+		setMedias();
+		setDesviosPadrao();
+	}
+	
+	public Double[] getMedias() {
+		return medias;
+	}
+	public void setMedias() {
+		this.medias = new Double[N_ATRIBUTOS];
+	}
+	public Double[] getDesviosPadrao() {
+		return desviosPadrao;
+	}
+	public void setDesviosPadrao() {
+		this.desviosPadrao = new Double[N_ATRIBUTOS]; 
+	}
+}
